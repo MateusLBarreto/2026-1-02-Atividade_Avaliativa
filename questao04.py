e265ecb12e67ad441de7e0282c05932759f765f1
MateusLBarreto <lemos.barreto@escolar.ifrn.edu.br>
2026-05-09 02:16:05 +0000

    Questao 4 realizada.

--- questao04.py
+++ questao04.py
@@ -1,4 +1,3 @@
 # Questao 4
-# Essa questao ira ser terminada em casa, ja que nao consegui achar uma solucao na sala de aula
 numero = input("Digite um valor:")
-casa = len(numero)
+print(len(numero))
